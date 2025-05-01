5a478a23be6f8ab38367c0a9a707b6abf51aebbc
fic <fic@NOTE26-S28>
2025-04-30 22:04:51 -0300

    criacao banco de dados

---
+++ ProjetoLivros/Context/LivrosContext.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoLivros.Models;
+
+namespace ProjetoLivros.Context
+{
+    public class LivrosContext : DbContext
+    {
+        // Cada tabela -> DbSet
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<TipoUsuario> TiposUsuarios { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Assinatura> Assinaturas { get; set; }
+
+        public LivrosContext(DbContextOptions<LivrosContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring
+            (DbContextOptionsBuilder optionsBuilder)
+        {
+            // String de Conexao
+            optionsBuilder.UseSqlServer("Data Source=NOTE26-S28\\SQLEXPRESS;Initial Catalog=Livros;User Id=sa;Password=Senai@134;TrustServerCertificate=true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuario>(
+                // Representacao da Tabela
+                entity =>
+                {
+                    // Primary Key
+                    entity.HasKey(u => u.UsuarioId);
+
+                    entity.Property(u => u.NomeCompleto)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                    entity.Property(u => u.Email)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                    //Email e Unico
+                    entity.HasIndex(u => u.Email)
+                    .IsUnique();
+
+                    entity.Property(u => u.Senha)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                    entity.Property(u => u.Telefone)
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                    entity.Property(u => u.DataCadastro)
+                    .IsRequired();
+
+                    entity.Property(u => u.DataAtualizacao)
+                    .IsRequired();
+
+                    entity.HasOne(u => u.TipoUsuario)
+                    .WithMany(t => t.Usuarios)
+                    .HasForeignKey(u => u.TipoUsuarioId)
+                    .OnDelete(DeleteBehavior.Cascade);
+                }                    
+            );
+        }
+    }
+}
